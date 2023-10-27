04fb29ce03104c62ff4c1da4f74c6574a61c9c25
Jonathan Ang <jonathanangml@gmail.com>
2023-10-27 14:06:22 -0700

    fix aprostrophe

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
                         LTA(NS) Jonathan
                     </h3>
                     <p>
-                        Hi there! I enlisted in Jan 2021 as part of the JC batch and ORD&aposd at the end of 2022. When I enlisted, I was sent to mono-armour and thought that I would be in Armour Infantry for the rest of my NS.
+                        Hi there! I enlisted in Jan 2021 as part of the JC batch and ORD&apos;d at the end of 2022. When I enlisted, I was sent to mono-armour and thought that I would be in Armour Infantry for the rest of my NS.
                         Yet, after BMT, I was posted to OCS where I joined the Army Wing. <br />
                         In another dramatic change, I was then posted to the Air Force Wing while I was in OCS and ended up commissioning as an Air Force Officer.
-                        It didn&apost stop there! After commissioning I was posted to an Air Force Battalion as a Logistics Officer...There were many changes in my journey and I hope that through this initiative I can help parents and enlistees understand more the many paths their NS journey can take.
+                        It didn&apos;t stop there! After commissioning I was posted to an Air Force Battalion as a Logistics Officer...There were many changes in my journey and I hope that through this initiative I can help parents and enlistees understand more the many paths their NS journey can take.
                     </p>
                 </div>
 
